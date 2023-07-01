523c80fcddd87e2d31c8019350c43faafabb4af7
KvDmtr <dimaskulich@gmail.com>
2023-07-01 20:43:58 +0300

    add default option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output fotmat')
+  .option('-f, --format <type>', 'output fotmat', 'stylish')
   .argument('<filePath1>')
   .argument('<filePath2>')
-  .action((filePath1, filePath2) => {
-    console.log(parser(filePath1, filePath2));
+  .action((filePath1, filePath2, options) => {
+    console.log(parser(filePath1, filePath2, options.format));
   });
 
 program.parse(process.argv);
