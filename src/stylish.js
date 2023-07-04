bf0bb1b87fedef967d7dc3c8c06f9ed6bd744b8a
KvDmtr <dimaskulich@gmail.com>
2023-07-04 22:36:44 +0300

    add logic for nested obj

--- src/stylish.js
+++ src/stylish.js
@@ -1,17 +1,31 @@
+import _ from 'lodash';
+
 const padLine = (depth) => `  ${' '.repeat(4).repeat(depth - 1)}`;
 const padBracket = (depth) => `${' '.repeat(4).repeat(depth)}`;
 const getLine = (key, value, char, depth) => `${padLine(depth)}${char}${key}: ${value}`;
 const wrapBrackets = (body, depth) => `{\n${body}\n${padBracket(depth)}}`;
 
-const chars = { added: '+ ', removed: '- ', unchanged: '  ' };
+const prepareValue = (value, depth) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const entrise = Object.entries(value);
+  const items = entrise.map(([key, val]) => getLine(key, prepareValue(val, depth + 1), '  ', depth + 1));
+  const body = items.join('\n');
+  return wrapBrackets(body, depth);
+};
 
 const parseDiff = (diff, depth) => {
   const items = diff.flatMap(({ key, value, state }) => {
+    const chars = { added: '+ ', removed: '- ', unchanged: '  ' };
     if (state === 'updated') {
-      return [getLine(key, value.oldValue, chars.removed, depth + 1),
-        getLine(key, value.newValue, chars.added, depth + 1)];
+      return [getLine(key, prepareValue(value.oldValue, depth + 1), chars.removed, depth + 1),
+        getLine(key, prepareValue(value.newValue, depth + 1), chars.added, depth + 1)];
     }
-    return getLine(key, value, chars[state], depth + 1);
+    if (state === 'nested') {
+      return getLine(key, parseDiff(value, depth + 1), '  ', depth + 1);
+    }
+    return getLine(key, prepareValue(value, depth + 1), chars[state], depth + 1);
   });
   const body = items.join('\n');
   return wrapBrackets(body, depth);
