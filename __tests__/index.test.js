e476df7df1a8a424bdace33fbec31fa1a96bf196
KvDmtr <dimaskulich@gmail.com>
2023-07-14 14:06:34 +0300

    add tests for plain & formatter json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,19 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import parser from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const isJson = (str) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
 test('json test', () => {
   const filename1 = fixturePath('file1.json');
@@ -22,4 +29,19 @@ test('yaml test', () => {
   const resultName = fixturePath('result_json.txt');
   const result = readFileSync(resultName, 'utf-8');
   expect(parser(fileName1, fileName2)).toBe(result);
+});
+
+test('plain test', () => {
+  const filename1 = fixturePath('file1.json');
+  const filename2 = fixturePath('file2.json');
+  const resultName = fixturePath('result_plain.txt');
+  const result = readFileSync(resultName, 'utf-8');
+  expect(parser(filename1, filename2, 'plain')).toBe(result);
+});
+
+test('formatter json', () => {
+  const filename1 = fixturePath('file1.json');
+  const filename2 = fixturePath('file2.json');
+  const result = parser(filename1, filename2, 'json');
+  expect(isJson(result)).toBeTruthy();
 });
